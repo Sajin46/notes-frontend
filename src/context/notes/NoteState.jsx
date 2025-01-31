3ba8d165957106197cbe48b621cc7a8ca9a09826
Sajin S <154986500+Sajin46@users.noreply.github.com>
2025-01-31 14:36:59 +0530

    Update NoteState.jsx

--- src/context/notes/NoteState.jsx
+++ src/context/notes/NoteState.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState = (props) => {
-    const host = "http://localhost:5555"
+    const host = "https://backend-cmhp.onrender.com"
     const notesInitials = [];
     const [notes, setNotes] = useState(notesInitials)
 
